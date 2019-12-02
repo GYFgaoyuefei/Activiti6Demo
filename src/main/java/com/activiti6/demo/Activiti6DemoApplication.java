bf5e843d0e94aecdeabd52757105ccf66f088c6c
liulu <yishuyu@eseasky.cn>
2019-12-02 18:38:32 +0800

    ttt

--- src/main/java/com/activiti6/demo/Activiti6DemoApplication.java
+++ src/main/java/com/activiti6/demo/Activiti6DemoApplication.java
@@ -1,5 +1,6 @@
 package com.activiti6.demo;
 
+import com.activiti6.demo.websocket.NettyServer;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,5 +11,12 @@ public class Activiti6DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Activiti6DemoApplication.class, args);
+
+
+        try {
+            new NettyServer(12345).start();
+        }catch(Exception e) {
+            System.out.println("NettyServerError:"+e.getMessage());
+        }
     }
 }
